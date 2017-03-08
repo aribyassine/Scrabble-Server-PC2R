a4c3cef6bd29bbe69365a439904b420c841241a6
Arib Yassine <aribyassine@hotmail.fr>
2017-03-08 05:43:03 +0100

    automated router

--- lib/pc2r/router.rb
+++ lib/pc2r/router.rb
@@ -9,66 +9,47 @@ module Pc2r
 
       loop do
         request = client.gets.chomp.force_encoding('UTF-8').to_s
-        route = parse request
-        params = request.match(@capture[route]).captures
+        begin
+          route  = parse(request)
+          params = request.match(@regexp[route]).captures
+          puts "route : '#{route.inspect}'  => params : #{params}"
+        rescue Exception => e
+          puts e.message
+          puts e.backtrace.inspect
+          next
+        end
+
         # Utilisateur authentifié
         if controller.authenticated?
           case route
-            when :sort    then controller.sort(params[0])
-            when :envoi   then controller.envoi(params[0])
-            when :penvoi  then controller.penvoi(params[0], params[1])
-            else
-              # invalide request
-              puts request
+            when :sort then
+              controller.sort(params[0])
+            when :envoi then
+              controller.envoi(params[0])
+            when :penvoi then
+              controller.penvoi(params[0], params[1])
           end
-=begin
-          # SORT/user/
-          if /^SORT\/#{@user}\/$/ =~ request
-            user = request.split('/').last
-            controller.sort(user)
-            # ENVOI/message/
-          elsif /^ENVOI\/#{@msg}\/$/ =~ request
-            msg = request.split('/').last
-            controller.envoi(msg)
-            # PENVOI/user/message/
-          elsif /^PENVOI\/#{@user}\/#{@msg}\/$/ =~ request
-            user = request.split('/')[1]
-            msg = request.split('/')[2]
-            controller.penvoi(user, msg)
-            # RECEPTION/message/
-          end
-=end
-          # Utilisateur non authentifié
         else
-          # CONNEXION/user/
-          if /^CONNEXION\/#{@user}\/$/ =~ request
-            user = request.split('/').last
-            controller.connexion(user)
-          end
+          # Utilisateur non authentifié
+          controller.connexion(params[0]) if route == :connexion
         end
+
       end
     end
 
     private
     @user = '[a-zA-Z]+'
-    @msg = '.+'
+    @msg  = '.+'
 
-    @match = {
-        :connexion    => /^CONNEXION\/#{@user}\/$/,
-        :sort         => /^SORT\/#{@user}\/$/,
-        :envoi        => /^ENVOI\/#{@msg}\/$/,
-        :penvoi       => /^PENVOI\/#{@user}\/#{@msg}\/$/,
-    }
-
-    @captures = {
-        :connexion    => /^CONNEXION\/(#{@user})\/$/,
-        :sort         => /^SORT\/(#{@user})\/$/,
-        :envoi        => /^ENVOI\/(#{@msg})\/$/,
-        :penvoi       => /^PENVOI\/(#{@user})\/(?<msg>#{@msg})\/$/,
+    @regexp = {
+        :connexion => /^CONNEXION\/(#{@user})\/$/,
+        :sort      => /^SORT\/(#{@user})\/$/,
+        :envoi     => /^ENVOI\/(#{@msg})\/$/,
+        :penvoi    => /^PENVOI\/(#{@user})\/(#{@msg})\/$/,
     }
 
     def self.parse(request)
-      found = @match.find { |k, v| v =~ request }
+      found = @regexp.find { |k, v| v =~ request }
       found.first unless found.nil?
     end
 
