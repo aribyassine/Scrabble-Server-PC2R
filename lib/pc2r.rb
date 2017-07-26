f1f900b5c6ce8657b5f3b747283a44cf7626c40a
Arib Yassine <aribyassine@hotmail.fr>
2017-07-26 02:24:14 +0200

    Update pc2r.rb

--- lib/pc2r.rb
+++ lib/pc2r.rb
@@ -6,18 +6,7 @@ require_relative 'pc2r/configutation'
 require 'pp'
 
 module Pc2r
-=begin
-  h = {}
-  h[:a] = 'lol'
-  h[:f] = 'wwwwwwww'
-  h[:e] = 'lofzfl'
-  h[:d] = 'lolr'
-  h[:c] = 'wwwwwwww'
-  h[:b] = 'lolrze'
 
-  pp h
-  pp h.max_by { |word| word.last.score }
-=end
   Configutation.load
   pp 'hello'.score()
   Thread.abort_on_exception = true
